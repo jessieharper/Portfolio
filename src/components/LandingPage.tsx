07feb8ada1186f1f4bb373d2b57118e20a93ec2e
Jessica Harper <jessicaharper@Jessicas-MacBook-Air.local>
2024-04-18 09:16:34 +0100

    add star background image

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -15,15 +15,16 @@ const LandingPage = (props: ICursor): JSX.Element => {
         }}
         className="h-auto flex sm:flex-row flex-col items-center relative"
       >
-        <div className="hidden sm:flex self-center justify-center align-middle border-4 border-text absolute left-[15%] rounded-full w-[500px] h-[500px] z-0">
+        <div className="hidden sm:flex self-center justify-center align-middle border-4 border-text absolute left-[15%] rounded-full w-[500px] h-[500px] z-10">
           <Earth />
         </div>
+
         <h2 className="max-w-[310px] sm:max-w-[350px] sm:text-9xl text-8xl font-extrabold z-10">
           Hiya, <br />
           World
         </h2>
         <span className="ml-6 flex flex-col max-w-[310px] sm:max-w-[350px] z-10 sm:p-2 sm:py-4 sm:rounded-2xl sm:bg-background sm:border-2 sm:border-text">
-          <p className="text-wrap">
+          <p className="text-wrap ">
             My name is Jessica, and I am a Sheffield-based Junior Software
             Developer, amateur animator, and burgeoning crazy cat lady. Welcome
             to my portfolio!
