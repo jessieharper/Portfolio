a794eb3d4e914fb37e076eafca8aca886d7cd4d5
Jessica Harper <jessicaharper@Jessicas-MacBook-Air.local>
2024-04-25 19:06:11 +0100

    create Projects component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,23 +10,23 @@ const About = (): JSX.Element => {
       className="flex flex-col h-[100vh] w-full mt-10 items-center"
     >
       <h2 className="text-5xl font-semibold p-4 mt-4">About Me</h2>
-      <article className="flex flex-col w-[90%] sm:w-[650px] m-auto justify-center bg-background">
+      <article className=" flex flex-col w-[90%] sm:w-[650px] m-auto justify-center bg-background">
         <ul className="flex items-center w-[100%]">
           {TabsData.map((tab, index) => (
             <li
               onClick={() => setActiveTab(index)}
-              className={`bg-card h-full text-xl py-4 p-6 w-[35%] text-center cursor-pointer border-t-2  border-text rounded-t-xl transition-all ease-in-out border-x-2 ${
-                index !== activeTab && "noise border-b-2"
-              } ${index === activeTab && "underline"} ${
-                tab.title === "Skills" && "border-r-2"
-              }`}
+              className={` bg-card h-full text-xl py-4 p-6 w-[35%] text-center cursor-pointer border-t-4  border-text rounded-t-xl transition-all ease-in-out border-x-2 ${
+                index !== activeTab && "noise border-b-4"
+              } ${index === activeTab && "underline font-semibold"} ${
+                tab.title === "Skills" && "border-r-4"
+              } ${tab.title === "About" && "border-l-4"}`}
               key={tab.title}
             >
               {tab.title}
             </li>
           ))}
         </ul>
-        <div className="bg-card dark:bg-background h-[560px] overflow-auto px-8 pt-8 w-full border-b-2 rounded-b-2xl border-x-2 border-text">
+        <div className="bg-card dark:bg-background h-[560px] overflow-auto px-8 pt-8 w-full border-b-4 rounded-b-2xl border-x-4 border-text">
           {TabsData.map((data, index) =>
             data.content.split("\\\n").map((line) => (
               <p
