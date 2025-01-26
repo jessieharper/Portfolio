020fbcd32f583155661461ca5c8fe29b6bbe6d01
Jess Harper <jessica@geniusdivision.com>
2025-01-26 13:13:53 +0000

    add box shadows to homepage cards

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,23 +3,34 @@ import TechStackLogos from "../components/TechStackLogos";
 import MyCV from "../assets/Jessica Harper CV.pdf";
 
 const LandingPage = (): JSX.Element => {
+  const cardContainer =
+    "shadow relative mt-12 flex flex-col w-full max-w-[21.5rem] z-10";
+  const cardClass =
+    "w-full h-full bg-card border-4 border-primary p-8 gap-8  flex flex-col";
   return (
     <section id="main" className="absolute top-0 h-full w-full ">
-      <div className="container flex items-center h-full">
+      <div className="container flex items-center w-full h-full">
         <div className="absolute inset-0 z-10">
           <Earth />
         </div>
 
-        <div className="relative mt-12 flex flex-col w-full max-w-[21.5rem] z-10">
-          <div className="absolute left-14 top-2 w-full h-full bg-primary opacity-75 z-0"></div>
+        <div className="flex w-full mx-auto justify-between">
+          <div className={cardContainer}>
+            <div className={cardClass}>
+              <h1 className="text-4xl text-center">Hiya, World!</h1>
+              <p className="text-xs leading-6 h-full">
+                My name is Jessica, and I am a Sheffield-based Junior Software
+                Developer, amateur animator, and burgeoning crazy cat lady.
+                Welcome to my portfolio!
+              </p>
+            </div>
+          </div>
 
-          <div className="relative w-full bg-card border-4 border-primary p-8 ml-12 gap-8 justify-center items-center flex flex-col">
-            <h1 className="text-4xl text-center">Hiya, World!</h1>
-            <p className="text-xs leading-6 h-full">
-              My name is Jessica, and I am a Sheffield-based Junior Software
-              Developer, amateur animator, and burgeoning crazy cat lady.
-              Welcome to my portfolio!
-            </p>
+          <div className={cardContainer}>
+            <div className={`${cardClass} font-depixel`}>
+              <h2>Tech Stack:</h2>
+              <p className="text-xs leading-6 h-full"></p>
+            </div>
           </div>
         </div>
       </div>
