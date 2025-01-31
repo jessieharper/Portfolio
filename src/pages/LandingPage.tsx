5286040b94a23658f2aef6c1d0f7cab33a5765d3
Jess Harper <jessica@geniusdivision.com>
2025-01-31 13:59:04 +0000

    update tech stack logos

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,34 +3,39 @@ import TechStackLogos from "../components/TechStackLogos";
 import MyCV from "../assets/Jessica Harper CV.pdf";
 
 const LandingPage = (): JSX.Element => {
-  const cardContainer =
-    "shadow relative mt-12 flex flex-col w-full max-w-[21.5rem] z-10";
-  const cardClass =
-    "w-full h-full bg-card border-4 border-primary p-8 gap-8  flex flex-col";
   return (
-    <section id="main" className="absolute top-0 h-full w-full ">
+    <section id="main" className="absolute top-0 h-full w-full px-12">
       <div className="container flex items-center w-full h-full">
         <div className="absolute inset-0 z-10">
           <Earth />
         </div>
 
-        <div className="flex w-full mx-auto justify-between">
-          <div className={cardContainer}>
-            <div className={cardClass}>
-              <h1 className="text-4xl text-center">Hiya, World!</h1>
-              <p className="text-xs leading-6 h-full">
-                My name is Jessica, and I am a Sheffield-based Junior Software
-                Developer, amateur animator, and burgeoning crazy cat lady.
-                Welcome to my portfolio!
-              </p>
-            </div>
+        <div className="flex w-full mx-auto justify-between items-end">
+          <div className="card">
+            <h1 className="text-4xl text-center leading-normal">
+              Hiya, World!
+            </h1>
+            <p className="text-xs leading-6 h-full">
+              My name is Jessica, and I am a Sheffield-based Junior Software
+              Developer, amateur animator, and burgeoning crazy cat lady.
+              Welcome to my portfolio!
+            </p>
           </div>
 
-          <div className={cardContainer}>
-            <div className={`${cardClass} font-depixel`}>
-              <h2>Tech Stack:</h2>
-              <p className="text-xs leading-6 h-full"></p>
-            </div>
+          <div className="card space-y-4">
+            <h2>Tech stack: </h2>
+
+            <TechStackLogos />
+
+            <a
+              href={MyCV}
+              download={"../assets/Jessica Harper CV.pdf"}
+              className="flex items-center justify-center"
+            >
+              <button className="text-body border-primary border-2 px-6 py-4 bg-no-repeat bg-gradient-to-r from-pink-500 to-yellow-500 w-full shadow-sm">
+                Download my CV
+              </button>
+            </a>
           </div>
         </div>
       </div>
