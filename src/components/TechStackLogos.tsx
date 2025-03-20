6501a8d76fa44fb8b3077d18dbf64b6dca9af234
Jess Harper <jessica@geniusdivision.com>
2025-03-20 10:54:20 +0000

    tweak home page layout

--- src/components/TechStackLogos.tsx
+++ src/components/TechStackLogos.tsx
@@ -4,7 +4,7 @@ const TechStackLogos = (): JSX.Element => {
   return (
     <div className="grid grid-cols-3 gap-6">
       {logos.map((logo) => (
-        <div key={logo} className="relative group flex justify-center">
+        <div key={logo} className="relative z-50 group flex justify-center">
           <div
             style={{
               maskImage: `url('/images/icons/${logo}.svg')`,
