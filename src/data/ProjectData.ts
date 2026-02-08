42ddb98a39d7f998775607a9c72b9009c6344bf1
Jessica Harper <joosicer@MacBookPro.powerhub>
2026-02-08 17:02:52 +0000

    update content

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -18,7 +18,7 @@ const projects: Project[] = [
     ],
     link: "",
     content:
-      "I’m currently building a mini game in Pico-8 where you play as a tiny alien on a mission to abduct cows. The catch? The cows must be collected in the correct order or you’ll lose points, and your rather impatient alien boss is timing the entire operation, so speed matters. Chaos, pressure, and cow logistics. Ehr. Mah. Gerd. I plan to make more mini games in Pico-8, so watch this space I guess.",
+      "I’m currently building a mini game in Pico-8 where you play as a tiny alien on a mission to abduct cows. The catch? The cows must be collected in the correct order or you’ll lose points, and your impatient alien boss is timing the entire operation, so speed matters. Chaos, pressure, and cow logistics. Ehr. Mah. Gerd. I plan to make more mini games in Pico-8, so watch this space I guess.",
   },
   {
     title: "Portfolio",
@@ -26,17 +26,18 @@ const projects: Project[] = [
     stack: "React, TypeScript, Blender, Tailwind, Three.js, GSAP",
     image: [],
     link: "",
-    content: "",
-  },
-  {
-    title: "MyLOS",
-    date: "WIP",
-    stack: "Laravel, Filament, PHP, Tailwind, MySQL",
-    image: [],
-    link: "",
     content:
-      "MyLOS (My Life Organisation System) is a personal project I’m building to explore Filament and solidify my Laravel skills. The goal is to create a centralised admin panel for managing projects, tasks, and personal budgets, with flexible organisation tools including lists, kanban boards, and budget trackers. This project has been an opportunity to deepen my understanding of Laravel’s ecosystem while developing a practical, scalable tool that I plan to evolve over time.",
+      "The website that you are currently on. This never-ending project has taken over a year of weekend tweaks, refactors, redesigns, and I-can't-be-bothered-to-do-this-anymore-and-I-want-to-do-something-else-s to push live 👍. Was it worth it? Sure, I learnt a lot along the way, especially about GSAP scroll triggers and Blender. Now that it's 'done', I can only envision 2 possible further courses of action: 1. Complete redesign, fuck it 2. Lean into the theme even further. More pixels. MORE.",
   },
+  // {
+  //   title: "MyLOS",
+  //   date: "WIP",
+  //   stack: "Laravel, Filament, PHP, Tailwind, MySQL",
+  //   image: [],
+  //   link: "",
+  //   content:
+  //     "MyLOS (My Life Organisation System) is a personal project I’m building to explore Filament and solidify my Laravel skills. The goal is to create a centralised admin panel for managing projects, tasks, and personal budgets, with flexible organisation tools including lists, kanban boards, and budget trackers. This project has been an opportunity to deepen my understanding of Laravel’s ecosystem while developing a practical, scalable tool that I plan to evolve over time.",
+  // },
   {
     title: "Sudowiz",
     date: "WIP",
@@ -47,7 +48,7 @@ const projects: Project[] = [
     ],
     link: "",
     content:
-      "Build a cross-platform Sudoku and puzzle app to strengthen mobile development skills and explore React Native while creating an engaging, user-friendly gaming experience. Customizable difficulty levels, ability to choose different clue patterns, daily challenge mode, player progress tracking, and social features for sharing results with friends. Planned future features include expanding to alternate Sudoku styles (e.g. Bomb Sudoku, Latin-Doku) to offer greater variety and replayability.",
+      "Are you a SudoWiz? What is a SudoWiz? And other questions. Find out on SudoWiz, the React Native Sudoku and puzzle app I'm building to strengthen my mobile development skills (if I ever release it that is lololol psyche). In an ideal world, this will include customizable difficulty levels, the ability to choose different clue patterns, a daily challenge mode, player progress tracking, and social features for sharing results with friends. Planned future features include expanding to alternate Sudoku styles (e.g. Bomb Sudoku, Latin-Doku) to offer greater variety and replayability. So watch this space I guess.",
   },
 
   {
@@ -57,7 +58,7 @@ const projects: Project[] = [
     image: [],
     link: "https://codelingo-by-tpwogitlou.netlify.app/",
     content:
-      "Design and develop a website aimed at helping children learn Python and JavaScript through interactive quizzes and gamified experiences. Built-in IDE-style coding sandbox for real-time practice, drag-and-drop quiz games, and a friendly, accessible interface inspired by Duolingo and Scratch. Collaborated with a team of fellow Northcoders to deliver an engaging educational platform within a tight deadline. Over the course of 8 days, we learned and applied a brand-new tech stack (TypeScript, Next.js, Tailwind, MongoDB) while building the app from scratch. The project showcased both our teamwork and ability to quickly adopt new technologies to meet ambitious goals.",
+      "My first ever group coding project (be nice). At the end of my coding bootcamp, I teamed up with a group of other Northcoders to form TPWOGITLOU (The People Who Occassionally Go Into the Leeds Office United) to design and develop a Scratch-like website aimed at helping children learn Python and JavaScript. We only had 8 days to learn and apply a brand-new tech stack while plannig and building the app from complete scratch. Obviously it's not perfect, but I had a lot of fun making it and I'm impressed at what we managed to accomplish as total noobs in just over a week. I also think this might be when my pixel art obsession began, just FYI.",
   },
 ];
 
